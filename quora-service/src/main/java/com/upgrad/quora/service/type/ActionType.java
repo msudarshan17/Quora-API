44c7be02701244d80f20f8390b820249f20410e6
msudarshan17 <33754721+msudarshan17@users.noreply.github.com>
2020-06-07 17:31:01 +0530

    Update ActionType.java

--- quora-service/src/main/java/com/upgrad/quora/service/type/ActionType.java
+++ quora-service/src/main/java/com/upgrad/quora/service/type/ActionType.java
@@ -1,11 +1,9 @@
 package com.upgrad.quora.service.type;
+//Enum for various actions for question and answer
 
-/**
- * Enum describing various various actions of the user for questions and answer
- * Based on the type of action specific exceptions will be thrown.
- */
 public enum ActionType {
-    //Question related actions
+
     EDIT_QUESTION, DELETE_QUESTION, CREATE_QUESTION, ALL_QUESTION, ALL_QUESTION_FOR_USER,
-    EDIT_ANSWER, DELETE_ANSWER, CREATE_ANSWER, GET_ALL_ANSWER_TO_QUESTION;
+    EDIT_ANSWER, DELETE_ANSWER, CREATE_ANSWER, GET_ALL_ANSWER_TO_QUESTION, GET_USER_DETAILS, DELETE_USER;
+
 }
